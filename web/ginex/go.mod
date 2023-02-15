a190577d7380515c325045aa426d636a4075ab2e
ynhj <542182536@qq.com>
2023-02-15 15:24:24 +0800

    feature: simple gpt stream

--- web/ginex/go.mod
+++ web/ginex/go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sashabaranov/go-gpt3 v1.1.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
--- web/ginex/main.go
+++ web/ginex/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	gogpt "github.com/sashabaranov/go-gpt3"
 	"io"
 	"log"
 	"net/http"
@@ -10,6 +14,15 @@ import (
 	"os/signal"
 	"time"
 )
+
+type SteamRequest struct {
+	Msg   string `form:"msg" binding:"required"`
+	Chats string `form:"chats" binding:"required"`
+}
+type Chat struct {
+	A string `form:"a"`
+	Q string `form:"q"`
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
@@ -23,24 +36,79 @@ func setupRouter() *gin.Engine {
 		c.String(200, "pong")
 	})
 	r.GET("/stream", func(c *gin.Context) {
-		chanStream := make(chan int, 10)
+		var request SteamRequest
+		err := c.ShouldBindQuery(&request)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		log.Println("request" + request.Msg)
+		var chats Chat
+		errs := json.Unmarshal([]byte(request.Chats), &chats)
+		if errs != nil {
+			fmt.Println("json unmarshal error:", errs)
+		}
+		//chanStream := make(chan int, 10)
+		//go func() {
+		//	defer close(chanStream)
+		//	for i := 0; i < 5; i++ {
+		//		chanStream <- i
+		//		time.Sleep(time.Second * 1)
+		//	}
+		//}()
+		//c.Stream(func(w io.Writer) bool {
+		//	if msg, ok := <-chanStream; ok {
+		//		c.SSEvent("message", msg)
+		//		if msg == 4 {
+		//			c.SSEvent("stop", msg)
+		//		}
+		//		return true
+		//	}
+		//	return false
+		//})
+		client := gogpt.NewClient("you token")
+		ctx := context.Background()
+
+		req := gogpt.CompletionRequest{
+			Model:     gogpt.GPT3Ada,
+			MaxTokens: 5,
+			Prompt:    request.Msg,
+			Stream:    true,
+		}
+		stream, err := client.CreateCompletionStream(ctx, req)
+		if err != nil {
+			return
+		}
 		go func() {
-			defer close(chanStream)
-			for i := 0; i < 5; i++ {
-				chanStream <- i
-				time.Sleep(time.Second * 1)
-			}
+			defer stream.Close()
 		}()
 		c.Stream(func(w io.Writer) bool {
-			if msg, ok := <-chanStream; ok {
-				c.SSEvent("message", msg)
-				return true
+			response, err := stream.Recv()
+			if errors.Is(err, io.EOF) {
+				fmt.Println("Stream finished")
+				c.SSEvent("stop", "")
+				return false
 			}
-			return false
+			if errors.Is(err, gogpt.ErrTooManyEmptyStreamMessages) {
+				// ... handle stream end
+				fmt.Println("Stream ErrTooManyEmptyStreamMessages")
+				c.SSEvent("stop", "")
+				return false
+			}
+
+			if err != nil {
+				fmt.Printf("Stream error: %v\n", err)
+				c.SSEvent("stop", "err")
+				return false
+			}
+
+			fmt.Printf("Stream response: %v\n", response)
+			c.SSEvent("message", response)
+			return true
 		})
 	})
 	return r
 }
+
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := setupRouter()
